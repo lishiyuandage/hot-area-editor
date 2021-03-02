e376ddb5a449c568188b7c407b90c7478315a45b
yuanyuan <yuanyuan.li@baobeituan.com>
2021-03-02 18:04:01 +0800

    init git

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+      <HotAreaEditor :url="url"></HotAreaEditor>
+  </div>
+</template>
+
+<script>
+import HotAreaEditor from './HotAreaEditor.vue'
+
+export default {
+  data(){
+      return {
+          url:'https://www.bing.com/th?id=OHR.VolcanoLlaima_ZH-CN3436127573_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=HpEdgeAn'
+      }
+  },
+  components: { HotAreaEditor },
+
+}
+</script>
+
+<style>
+</style>
---
+++ HotAreaEditor.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    @mousemove="move"
+    @mouseup="up"
+    :style="
+      'background-image:url(' +
+      url +
+      ');height:' +
+      height +
+      'px;width:' +
+      width +
+      'px;'
+    "
+  >
+    <div v-for="(area, index) in areas" :key="index">
+      {{ area }}
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * {
+ *  actName:'',
+ *  version:1.0,
+ *  img:{
+ *     url:http://baidu.com/logo.jpg,
+ *     height:50,
+ *     width:200,
+ *     areas:[{
+ *         type: 'rect',
+ *         x: 0,
+ *         y: 0,
+ *         width: this.realImage.width,
+ *         height: this.realImage.height,
+ *         url: '',
+ *         clazz: '',
+ *         detail: ''
+ *     }]
+ *    }
+ * }
+ */
+const VERSION = 1.0;
+
+export default {
+  data() {
+    return {
+      version: VERSION,
+      initCall: null,
+      downPoint: { x: 0, y: 0 },
+      height: 0,
+      width: 0,
+      areas: [],
+    };
+  },
+
+  name: "HotAreaEditor",
+
+  props: {
+    url: { type: String,default:"" },
+    json: {
+      type: Object,
+      default: () => {
+        return null;
+      },
+    },
+  },
+  mounted() {
+    console.log('------------热区编辑器初始化-------------')
+    this.initArea()
+  },
+  methods: {
+    initArea(){
+        if(this.json != null){
+            console.log("hot-area-editor init json")
+
+        }else if(this.url != ""){
+            console.log("hot-area-editor init url")
+
+        }
+    },
+    exportCode() {},
+    move() {},
+    up() {},
+  },
+};
+</script>
+
+<style>
+</style>
