f4066f3a9f1e482c2163e253db1d3962b7222826
yuanyuan <yuanyuan.li@baobeituan.com>
2021-03-03 18:19:20 +0800

    xxx

--- App.vue
+++ App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box">
-      <HotAreaEditor :url="url"></HotAreaEditor>
+      <HotAreaEditor :url="url" ref="editor1" shape="circle"></HotAreaEditor>
+      <button @click="addArea">添加热区</button>
   </div>
 </template>
 
@@ -10,11 +11,17 @@ import HotAreaEditor from './HotAreaEditor.vue'
 export default {
   data(){
       return {
-          url:'https://www.bing.com/th?id=OHR.VolcanoLlaima_ZH-CN3436127573_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=HpEdgeAn'
+          add:false,
+          url:'https://th.bing.com/th/id/R0313037203832ca3cb56be092a71fe76?rik=SSjYzMbEAuZVkQ&riu=http%3a%2f%2fwww.pooban.com%2fimages%2f201511%2fgoods_img%2f2070_G_1447002951605.png&ehk=kPYBvHm58hk6Jyl%2fC5qCTWb5qMeDK55twe54ZjqwOes%3d&risl=&pid=ImgRaw'
       }
   },
   components: { HotAreaEditor },
-
+  methods:{
+    addArea(){
+      console.log(this)
+      this.$refs.editor1.addArea()
+    }
+  }
 }
 </script>
 
--- HotAreaEditor.vue
+++ HotAreaEditor.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    class="hot-editor-area"
     @mousemove="move"
     @mouseup="up"
     :style="
@@ -12,8 +13,48 @@
       'px;'
     "
   >
-    <div v-for="(area, index) in areas" :key="index">
-      {{ area }}
+    <div class="areas" v-for="(area, index) in areas" :key="index">
+      <div
+        class="area-box"
+        v-if="area.type == 'rect'"
+        @mousedown="down(index, 1)"
+        :style="
+          'left:' +
+          area.point[0].x +
+          'px; top:' +
+          area.point[0].y +
+          'px; width:' +
+          (area.point[1].x - area.point[0].x) +
+          'px; height:' +
+          (area.point[1].y - area.point[0].y) +
+          'px;'
+        "
+      >
+        <div class="del-box-btn" @click="delArea(index)"></div>
+        <div @mousedown="down(index, 2)" class="right-box-btn"></div>
+        <div @mousedown="down(index, 3)" class="bottom-box-btn"></div>
+      </div>
+      <div
+        class="area-box"
+        v-if="area.type == 'circle'"
+        @mousedown="down(index, 1)"
+        :style="
+          'border-radius:' +
+          area.radius +
+          'px;left:' +
+          area.point[0].x +
+          'px; top:' +
+          area.point[0].y +
+          'px; width:' +
+          area.radius * 2 +
+          'px; height:' +
+          area.radius * 2 +
+          'px;'
+        "
+      >
+        <div class="del-box-btn" @click="delArea(index)"></div>
+        <div @mousedown="down(index, 2)" class="right-box-btn"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,18 +62,16 @@
 <script>
 /**
  * {
- *  actName:'',
+ *  name:'',
  *  version:1.0,
- *  img:{
+ *  image:{
  *     url:http://baidu.com/logo.jpg,
  *     height:50,
  *     width:200,
  *     areas:[{
  *         type: 'rect',
- *         x: 0,
- *         y: 0,
- *         width: this.realImage.width,
- *         height: this.realImage.height,
+ *         point:[{}]
+ *         radius:null,
  *         url: '',
  *         clazz: '',
  *         detail: ''
@@ -51,13 +90,17 @@ export default {
       height: 0,
       width: 0,
       areas: [],
+      editMode: 0,
+      currentArea: 0,
     };
   },
 
   name: "HotAreaEditor",
 
   props: {
-    url: { type: String,default:"" },
+    url: { type: String, default: "" },
+    shape: { type: String, default: "rect" },
+    add: { type: Boolean, default: false },
     json: {
       type: Object,
       default: () => {
@@ -65,26 +108,182 @@ export default {
       },
     },
   },
+
   mounted() {
-    console.log('------------热区编辑器初始化-------------')
-    this.initArea()
+    console.log(
+      "------------热区编辑器初始化verison:" + this.version + "-------------"
+    );
+    this.initArea();
   },
+
+  watch: {
+    url(oldVal, val) {
+      console.log(oldVal + "-----------", val);
+      if (oldVal != "" && oldVal != val) {
+        this.changeUrl();
+      }
+    },
+    shape(val) {
+      if (val != "rect" && val != "circle" && val != "poly") {
+        throw new Error(
+          "请设置合法的形状;the shape：" + val + " is not support."
+        );
+      }
+    },
+  },
+
   methods: {
-    initArea(){
-        if(this.json != null){
-            console.log("hot-area-editor init json")
-
-        }else if(this.url != ""){
-            console.log("hot-area-editor init url")
-
-        }
+    delArea(index) {
+      let i = Number(index);
+      this.areas.splice(i, 1);
+    },
+    down(index, type) {
+      if (event.button == 0 && event.buttons == 1) {
+        console.log(event)
+        this.currentArea = index;
+        this.editMode = type;
+        this.downPoint.x = event.x;
+        this.downPoint.y = event.y;
+      }
+      event.preventDefault()
+      event.stopPropagation()
+    },
+    changeUrl() {
+      let image = new Image();
+      image.src = this.url;
+      image.onload = () => {
+        this.height = image.height;
+        this.width = image.width;
+      };
+    },
+    initArea() {
+      if (this.json != null) {
+        console.log("hot-area-editor init json");
+        this.url = this.json.image.url;
+        this.height = this.json.image.height;
+        this.width = this.json.image.width;
+        this.areas = JSON.parse(JSON.stringify(this.json.image.areas));
+      } else if (this.url != "") {
+        console.log("hot-area-editor init url");
+        this.changeUrl();
+      }
+    },
+    addArea() {
+      console.log("-----------");
+      this.areas.push({
+        type: this.shape,
+        point: [
+          { x: 0, y: 0 },
+          { x: 50, y: 50 },
+        ],
+        radius: 25,
+        url: "",
+        clazz: "",
+        detail: "",
+      });
     },
     exportCode() {},
-    move() {},
-    up() {},
+    move() {
+      if (event.button == 0 && event.buttons == 1 && this.editMode != 0) {
+        let xT = event.x - this.downPoint.x
+        let yT = event.y - this.downPoint.y
+
+        let currentAreaEle=this.areas[this.currentArea]
+
+        if (this.editMode == 1) {
+          console.log(currentAreaEle)
+          this.areas[this.currentArea].point[0].x += xT
+          this.areas[this.currentArea].point[0].y += yT
+          this.areas[this.currentArea].point[1].x += xT
+          this.areas[this.currentArea].point[1].y += yT
+        } else if (this.editMode == 2) {
+          if (currentAreaEle.type == 'rect') {
+            this.areas[this.currentArea].point[1].x += xT
+          } else if(currentAreaEle.type == 'circle') {
+            this.areas[this.currentArea].point[0].x -= xT
+            this.areas[this.currentArea].point[0].y -= xT
+            this.areas[this.currentArea].radius +=xT            
+          }
+        } else if (this.editMode == 3) {
+          if (currentAreaEle.type == 'rect') {
+            this.areas[this.currentArea].point[1].y += yT
+          } else if(currentAreaEle.type == 'circle') {
+            this.areas[this.currentArea].radius +=xT
+          }          
+        }
+        this.downPoint.x = event.x
+        this.downPoint.y = event.y
+      }
+    },
+    up() {
+      if (event.button == 0 && event.buttons == 1) {
+        this.currentArea = 0;
+        this.editMode = 1; 
+        this.downPoint.x = event.x;
+        this.downPoint.y = event.y;
+      }
+    },
   },
 };
 </script>
 
 <style>
+.hot-editor-area {
+  position: relative;
+}
+.area-box {
+  border: 1px rgb(150, 235, 238) solid;
+  background: rgba(150, 235, 238, 0.3);
+  cursor: move;
+  position: absolute;
+}
+.del-box-btn {
+  position: absolute;
+  cursor: pointer;
+  top: -7px;
+  left: calc(50% - 7px);
+  border: black 1px solid;
+  background: #fff;
+  line-height: 12px;
+  width: 12px;
+  height: 12px;
+  text-align: center;
+  vertical-align: center;
+  border-radius: 100%;
+}
+.del-box-btn::after{
+  content: '×';
+  position: absolute;
+  left: 0px;
+}
+
+.right-box-btn {
+  font-size: smaller;
+  position: absolute;
+  top: calc(50% - 7px);
+  left: calc(100% - 7px);
+  border: black 1px solid;
+  background: #fff;
+  width: 12px;
+  cursor: e-resize;
+  height: 12px;
+  text-align: center;
+  vertical-align: center;
+  border-radius: 100%;
+}
+
+.bottom-box-btn {
+  font-size: smaller;
+  position: absolute;
+  top: calc(100% - 7px);
+  left: calc(50% - 7px);
+  border: black 1px solid;
+  background: #fff;
+  cursor: n-resize;
+  width: 12px;
+  height: 12px;
+  text-align: center;
+  vertical-align: center;
+  border-radius: 100%;
+}
 </style>
