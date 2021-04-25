2160c493b3be3f886d96638043d47f1d103e7f86
luoshiwei <273247516@qq.com>
2021-04-25 17:27:45 +0800

    添加多边形功能

--- App.vue
+++ App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="box">
       <div class="box-editor">
-        <HotAreaEditor :url="url" ref="editor1" :shape="shape"></HotAreaEditor>
+        <HotAreaEditor :url="url" ref="editor1" :shape="shape" :svgEditorOpen="svgEditorOpen"></HotAreaEditor>
       </div>
       <div class="box-options">
         <input type="text" v-model="url" placeholder="图片链接">
         <button @click="exportCode">导出</button>
+        <button @click="svgEditorOpen = !svgEditorOpen">打开/关闭多边形编辑器</button>
         <button @click="addArea">添加热区</button>
         <select v-model="shape">
           <option value="circle">圆</option>
@@ -26,6 +27,7 @@ export default {
           html:'',
           shape:'circle',
           add:false,
+          svgEditorOpen:false,
           url:'https://th.bing.com/th/id/R0313037203832ca3cb56be092a71fe76?rik=SSjYzMbEAuZVkQ&riu=http%3a%2f%2fwww.pooban.com%2fimages%2f201511%2fgoods_img%2f2070_G_1447002951605.png&ehk=kPYBvHm58hk6Jyl%2fC5qCTWb5qMeDK55twe54ZjqwOes%3d&risl=&pid=ImgRaw'
       }
   },
--- HotAreaEditor.vue
+++ HotAreaEditor.vue
@@ -38,10 +38,30 @@
           @mousedown="noBubble"
           @mouseup="noBubble"
         >
-          <input class="area-input" type="text" v-model="area.url" placeholder="链接/url" />
-          <input class="area-input" type="text" v-model="area.detail" placeholder="详细说明/alt" />
-          <input class="area-input" type="text" v-model="area.clazz" placeholder="类/class" />
-          <select class="area-input" type="text" v-model="area.target" placeholder="打开方式/target">
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.url"
+            placeholder="链接/url"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.detail"
+            placeholder="详细说明/alt"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.clazz"
+            placeholder="类/class"
+          />
+          <select
+            class="area-input"
+            type="text"
+            v-model="area.target"
+            placeholder="打开方式/target"
+          >
             <option value="_blank">_blank</option>
             <option value="_self">_self</option>
             <option value="_parent">_parent</option>
@@ -78,10 +98,30 @@
           @mousedown="noBubble"
           @mouseup="noBubble"
         >
-          <input class="area-input" type="text" v-model="area.url" placeholder="链接/url" />
-          <input class="area-input" type="text" v-model="area.detail" placeholder="详细说明/alt" />
-          <input class="area-input" type="text" v-model="area.clazz" placeholder="类/class" />
-          <select class="area-input" type="text" v-model="area.target" placeholder="打开方式/target">
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.url"
+            placeholder="链接/url"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.detail"
+            placeholder="详细说明/alt"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.clazz"
+            placeholder="类/class"
+          />
+          <select
+            class="area-input"
+            type="text"
+            v-model="area.target"
+            placeholder="打开方式/target"
+          >
             <option value="_blank">_blank</option>
             <option value="_self">_self</option>
             <option value="_parent">_parent</option>
@@ -92,22 +132,74 @@
         <div @mousedown="down(index, 2)" class="right-box-btn"></div>
       </div>
       <!-- 多边形 -->
-      <svg
-        :width="width"
-        :height="height"
+      <div
         v-if="area.type == 'poly'"
+        class="area-box"
+        :style="
+          'top:' +
+          area.point.reduce((i, j) => (i.y < j.y ? i : j)).y +
+          'px;left:' +
+          area.point.reduce((i, j) => (i.x < j.x ? i : j)).x +
+          'px'
+        "
       >
-      <polygon @mousedown="down(index, 1)"  @click="openOrCloseAttr"  points="200,10 250,190 160,210" style="fill:rgba(150, 235, 238, 0.3);stroke:rgb(150, 235, 238);stroke-width:1"/>
-       <div
+        <svg
+          class="area-box-svg"
+          :width="
+            area.point.reduce((i, j) => (i.x > j.x ? i : j)).x -
+            area.point.reduce((i, j) => (i.x < j.x ? i : j)).x
+          "
+          :height="
+            area.point.reduce((i, j) => (i.y > j.y ? i : j)).y -
+            area.point.reduce((i, j) => (i.y < j.y ? i : j)).y
+          "
+        >
+          <polygon
+            @mousedown="down(index, 1)"
+            @click="openOrCloseAttr"
+            :points="
+              area.point
+                .map(
+                  (i) =>
+                    i.x -
+                    area.point.reduce((i, j) => (i.x < j.x ? i : j)).x +
+                    ',' +
+                    (i.y - area.point.reduce((i, j) => (i.y < j.y ? i : j)).y)
+                )
+                .join(',')
+            "
+          />
+        </svg>
+        <div
           class="area-input area-input-group"
           @click="noBubble"
           @mousedown="noBubble"
           @mouseup="noBubble"
         >
-          <input class="area-input" type="text" v-model="area.url" placeholder="链接/url" />
-          <input class="area-input" type="text" v-model="area.detail" placeholder="详细说明/alt" />
-          <input class="area-input" type="text" v-model="area.clazz" placeholder="类/class" />
-          <select class="area-input" type="text" v-model="area.target" placeholder="打开方式/target">
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.url"
+            placeholder="链接/url"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.detail"
+            placeholder="详细说明/alt"
+          />
+          <input
+            class="area-input"
+            type="text"
+            v-model="area.clazz"
+            placeholder="类/class"
+          />
+          <select
+            class="area-input"
+            type="text"
+            v-model="area.target"
+            placeholder="打开方式/target"
+          >
             <option value="_blank">_blank</option>
             <option value="_self">_self</option>
             <option value="_parent">_parent</option>
@@ -115,9 +207,14 @@
           </select>
         </div>
         <div class="del-box-btn" @click="delArea(index)"></div>
-      </svg>
+      </div>
       <!-- end -->
     </div>
+    <!-- svg editor -->
+    <svg v-if="svgEditorOpen" class="svg-area-editor" :width="width" :height="height" @click="addPoint">
+      <polygon :points="svgPoint.map( i => i.x + ',' + i.y ).join(',')"/>
+      <rect v-for="(point, index) in svgPoint" :key="index" width="4" height="4" :y="point.y" :x="point.x"  @click="delPoint(index)"/>
+    </svg>
   </div>
 </template>
 
@@ -147,6 +244,7 @@ const VERSION = 1.1;
 export default {
   data() {
     return {
+      
       version: VERSION,
       initCall: null,
       downPoint: { x: 0, y: 0 },
@@ -154,13 +252,15 @@ export default {
       width: 0,
       areas: [],
       editMode: 0,
-      currentArea: 0
+      svgPoint:[],
+      currentArea: 0,
     };
   },
 
   name: "HotAreaEditor",
 
   props: {
+    svgEditorOpen:{type:Boolean,default:false},
     url: { type: String, default: "" },
     shape: { type: String, default: "rect" },
     add: { type: Boolean, default: false },
@@ -168,8 +268,8 @@ export default {
       type: Object,
       default: () => {
         return null;
-      }
-    }
+      },
+    },
   },
 
   mounted() {
@@ -193,19 +293,24 @@ export default {
           "请设置合法的形状;the shape：" + val + " is not support."
         );
       }
-    }
+    },
   },
 
   methods: {
     openOrCloseAttr() {
-      let ele = null
-      if(event.target.nodeName=='polygon'){
-          ele = event.target.parentElement.querySelector(".area-input-group")
-      }else{
-        ele = event.target.querySelector(".area-input-group")
+      let ele = null;
+      if (event.target.nodeName == "polygon") {
+        ele =
+          event.target.parentElement.querySelector(".area-input-group") ||
+          event.target.parentElement.parentElement.querySelector(
+            ".area-input-group"
+          );
+      } else {
+        ele = event.target.querySelector(".area-input-group");
       }
-      let display = ele.style .display;
-      ele.style.display = display == "block" ? "none": "block"
+      let display = ele.style.display;
+      console.log(display);
+      ele.style.display = display == "block" ? "none" : "block";
       event.stopPropagation();
     },
     noBubble() {
@@ -246,23 +351,52 @@ export default {
         this.changeUrl();
       }
     },
+    // -----------------svg-----
+    openOrCloseSvgEditor(){
+      let display = this.$el.querySelector(".svg-area-editor").style.display
+      this.$el.querySelector(".svg-area-editor").style.display = display == 'block' ? 'none' : 'block'
+    },
+    delPoint(index){
+      let i = Number(index);
+      this.svgPoint.splice(i, 1);
+      event.stopPropagation();
+    },
+    addPoint(){
+      let rect =this.$el.getBoundingClientRect()
+      let continarX = rect.x
+      let continarY = rect.y
+      let eventX = event.x
+      let eventY = event.y
+      this.svgPoint.push({
+        x:eventX-continarX,
+        y:eventY-continarY
+      })
+    },
+    // -----------------svg--end----
     addArea() {
+      let point = [
+        { x: 0, y: 0 },
+        { x: 50, y: 50 },
+      ];
+      if (this.shape == "poly") {
+        if(this.svgPoint.length < 3){
+          throw new Error("多边形不能少与3个点;poly must have over 3 points.")
+        }
+        point = this.svgPoint
+        this.svgPoint = []
+      }
       this.areas.push({
         type: this.shape,
-        point: [
-          { x: 0, y: 0 },
-          { x: 50, y: 50 }
-        ],
+        point: point,
         radius: 25,
         url: "",
         clazz: "",
         detail: "",
-        target: "_blank"
+        target: "_blank",
       });
     },
     exportCode() {
       let code = Math.random();
-
       let json = {
         name: "hot-area-" + code,
         version: this.version,
@@ -270,8 +404,8 @@ export default {
           url: this.url,
           height: this.height,
           width: this.width,
-          areas: JSON.parse(JSON.stringify(this.areas))
-        }
+          areas: JSON.parse(JSON.stringify(this.areas)),
+        },
       };
 
       let html =
@@ -290,21 +424,7 @@ export default {
         code +
         `'>`;
       for (let area of json.image.areas) {
-        let coords =
-          area.type == "rect"
-            ? area.point[0].x +
-              "," +
-              area.point[0].y +
-              "," +
-              area.point[1].x +
-              "," +
-              area.point[1].y
-            : area.point[0].x +
-              area.radius +
-              "," +
-              (area.point[0].y + area.radius) +
-              "," +
-              area.radius;
+        let coords = this.getCoords(area);
         html +=
           `<area shape='` +
           area.type +
@@ -324,8 +444,35 @@ export default {
 
       return {
         json: json,
-        html: html
+        html: html,
       };
+    },
+    getCoords(area) {
+      let coords = "";
+      switch (area.type) {
+        case "rect":
+          coords =
+            area.point[0].x +
+            "," +
+            area.point[0].y +
+            "," +
+            area.point[1].x +
+            "," +
+            area.point[1].y;
+          break;
+        case "poly":
+          coords = area.point.map((i) => i.x + "," + i.y).join(",");
+          break;
+        default:
+          coords =
+            area.point[0].x +
+            area.radius +
+            "," +
+            (area.point[0].y + area.radius) +
+            "," +
+            area.radius;
+      }
+      return coords;
     },
     move() {
       if (event.button == 0 && event.buttons == 1 && this.editMode != 0) {
@@ -335,10 +482,17 @@ export default {
         let currentAreaEle = this.areas[this.currentArea];
 
         if (this.editMode == 1) {
-          this.areas[this.currentArea].point[0].x += xT;
-          this.areas[this.currentArea].point[0].y += yT;
-          this.areas[this.currentArea].point[1].x += xT;
-          this.areas[this.currentArea].point[1].y += yT;
+          if (currentAreaEle.type == "poly") {
+            for (let point of this.areas[this.currentArea].point) {
+              point.x += xT;
+              point.y += yT;
+            }
+          } else {
+            this.areas[this.currentArea].point[0].x += xT;
+            this.areas[this.currentArea].point[0].y += yT;
+            this.areas[this.currentArea].point[1].x += xT;
+            this.areas[this.currentArea].point[1].y += yT;
+          }
         } else if (this.editMode == 2) {
           if (currentAreaEle.type == "rect") {
             this.areas[this.currentArea].point[1].x += xT;
@@ -365,8 +519,8 @@ export default {
         this.downPoint.x = event.x;
         this.downPoint.y = event.y;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -385,6 +539,31 @@ export default {
 }
 .hot-editor-area {
   position: relative;
+}
+.svg-area-editor{
+  position: absolute;
+  left: 0px;
+  top:0px;
+  background: rgba(128, 18, 86, 0.3);
+}
+.svg-area-editor rect{
+  position: absolute;
+  fill:rgb(255, 255, 255);
+  stroke-width:1;
+  stroke:rgb(0,0,0)
+}
+.svg-area-editor polygon{
+  fill: rgba(150, 235, 238, 0.3);
+  stroke: rgb(150, 235, 238);
+  stroke-width: 1;
+}
+.area-box-svg polygon{
+  fill: rgba(150, 235, 238, 0.3);
+  stroke: rgb(150, 235, 238);
+  stroke-width: 1;
+}
+.area-box-svg {
+  position: absolute;
 }
 .area-box {
   border: 1px rgb(150, 235, 238) solid;
